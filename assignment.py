b20fb2bb9d82d3d1c53708d76c25e5a9fde64ef2
Aqsa sarfraz <Rikzaaqsa@gmail.com>
2019-10-05 21:20:52 +0500

    My First commit

---
+++ assignment.py
@@ -0,0 +1,16 @@
+x=6
+print (x)
+y=x+7
+print (y)
+
+print ('You must be the legendry god that comes from the sky')
+print ('We have been waiting for you a long time')
+print ('Our legend says you will be very tasty with mustard')
+print ('We will have a feast tonight unless you say')
+
+x=43
+x=x+1
+print (x)
+print ('I hate you python')
+print ('good bye')
+print ('Hello World')
