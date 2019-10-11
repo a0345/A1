332637473b7e8ba4640a0ea1e2bf6b6740f8abb6
Aqsa sarfraz <Rikzaaqsa@gmail.com>
2019-10-11 22:30:29 +0500

    my first commit

---
+++ 13.py
@@ -0,0 +1,38 @@
+print (1)
+x = 2
+print (1)
+x = 2
+print (x)
+
+
+x = 10
+y = 15
+print (x + y)
+
+x = '100'
+y = '150'
+print (x + y)
+
+x = '4'
+y = '3'
+print (x * y)
+
+a = 35.0
+b= 12.50
+c = a * b
+print (c)
+
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print (pay)
+
+minute = 59
+minute/60
+
+>>> quotient = 7//3
+>>> print (quotient)
+
+>>> remainder = 7%3
+>>> print (remainder)
+
